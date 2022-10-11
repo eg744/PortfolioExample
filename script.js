d70ac1f4754ecd59ad9f1f98e8cb4db1b7d71176
Elijah Gillit <egillit@gmail.com>
2022-10-11 16:13:14 -0600

    responsive nav menu, hamburger button

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const navbarToggle = document.querySelector('.hamburger');
+
+	const primaryNavbar = document.querySelector('.primary-navigation');
+
+	// Toggle mobile menu
+	navbarToggle.addEventListener('click', () => {
+		navbarToggle.classList.toggle('change');
+
+		const isVisible = primaryNavbar.getAttribute('data-visible');
+		if (!isVisible) {
+			primaryNavbar.setAttribute('data-visible', true);
+			navbarToggle.setAttribute('data-expanded', true);
+		} else {
+			primaryNavbar.setAttribute('data-visible', false);
+			navbarToggle.setAttribute('data-expanded', false);
+		}
+	});
+});
