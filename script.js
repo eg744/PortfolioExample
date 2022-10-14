c5ebe72cf770a2b41e005446be1771c0362a4997
Elijah Gillit <egillit@gmail.com>
2022-10-13 19:46:17 -0600

    styling animated tunnel effect

--- script.js
+++ script.js
@@ -18,4 +18,37 @@ document.addEventListener('DOMContentLoaded', () => {
 			navbarToggle.setAttribute('data-expanded', false);
 		}
 	});
+
+	// Insert new html element inside existing element
+	function insertAfter(newNode, referenceNode) {
+		referenceNode.parentNode.insertBefore(
+			newNode,
+			referenceNode.nextSibling
+		);
+	}
+
+	function setCssTunnelStyle() {
+		let style = document.createElement('style');
+		style.innerHTML = '.';
+	}
+
+	function animateCssTunnel() {
+		const tunnelStyle = {};
+
+		// const tunnelFrame = newDiv.classList.add('animate-frame');
+		const cssTunnelElement = document.getElementById(
+			'animate-css-container'
+		);
+
+		for (let i = 0; i < 2; i++) {
+			const newFrame = document.createElement('div');
+			newFrame.style.cssText;
+
+			// newFrame.classList.add('animate-frame');
+
+			cssTunnelElement.appendChild(newFrame);
+			console.log(i);
+		}
+	}
+	animateCssTunnel();
 });
