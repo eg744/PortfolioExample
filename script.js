3dd6ccabe4fdb403841f778b9cbaec95a463e63f
Elijah Gillit <egillit@gmail.com>
2023-04-05 13:31:18 -0600

    remove some unused JS, contact form fields required

--- script.js
+++ script.js
@@ -59,76 +59,47 @@ document.addEventListener('DOMContentLoaded', () => {
 		aboutMeArrow.classList.toggle('arrow-opened');
 	});
 
-	// Email copy
-	// document.getElementById('copy-mail').addEventListener('click', () => {
-	// 	// add email to clipboard
-	// 	console.log(navigator);
-	// 	navigator.clipboard.writeText(
-	// 		document.getElementById('copy-mail-input').value
+	// CSS "tunnel" just for fun
+	// function createCssTunnel(
+	// 	width,
+	// 	height,
+	// 	borderRadius,
+	// 	border,
+	// 	boxShadowOuter,
+	// 	boxShadowInner,
+	// 	rotateDeg,
+	// 	translateY,
+	// 	translateZ,
+	// 	opacity,
+	// 	animationDelay
+	// ) {
+	// 	const cssTunnelElement = document.getElementById(
+	// 		'tunnel-css-container'
 	// 	);
 
-	// 	// add class
-	// 	document.getElementById('copy-mail').classList.add('copied');
+	// 	for (let i = 0; i < 12; i++) {
+	// 		const newFrame = document.createElement('div');
 
-	// 	setTimeout(() => {
-	// 		document.getElementById('copy-mail').classList.remove('copied');
-	// 	}, 3000);
-	// });
+	// 		newFrame.classList.add('animate-frame');
+	// 		newFrame.style.cssText = `width: ${width}%;
+	// 			height: ${height}%;
+	// 			border-radius: ${borderRadius}%;
+	// 			position: absolute;
+	// 			border: ${border};
 
-	// CSS "tunnel" just for fun
-	function createCssTunnel(
-		width,
-		height,
-		borderRadius,
-		border,
-		boxShadowOuter,
-		boxShadowInner,
-		rotateDeg,
-		translateY,
-		translateZ,
-		opacity,
-		animationDelay
-	) {
-		const cssTunnelElement = document.getElementById(
-			'tunnel-css-container'
-		);
+	// 			box-shadow: ${boxShadowOuter},
+	// 			${boxShadowInner};
+	// 			transform: rotate(${rotateDeg}deg) translateY(${translateY}px) translateZ(${translateZ}px);
+	// 			opacity: ${opacity};
+	// 			animation-delay:${animationDelay}s;`;
 
-		for (let i = 0; i < 12; i++) {
-			const newFrame = document.createElement('div');
-
-			newFrame.classList.add('animate-frame');
-			newFrame.style.cssText = `width: ${width}%;
-				height: ${height}%;
-				border-radius: ${borderRadius}%;
-				position: absolute;
-				border: ${border};
-
-				box-shadow: ${boxShadowOuter},
-				${boxShadowInner};
-				transform: rotate(${rotateDeg}deg) translateY(${translateY}px) translateZ(${translateZ}px);
-				opacity: ${opacity};
-				animation-delay:${animationDelay}s;`;
-
-			cssTunnelElement.appendChild(newFrame);
-			width = width + 5;
-			// rotateDeg = rotateDeg - 5;
-			// translateY = translateY + 50;
-			translateZ = translateZ - 10;
-			opacity = opacity - 0.05;
-			animationDelay = animationDelay + 0.2;
-		}
-	}
-	// createCssTunnel(
-	// 	100,
-	// 	100,
-	// 	100,
-	// 	'5px #e68608 solid',
-	// 	'0px 0px 20px #b45c0a, inset 0px 0px 20px #b45c0a',
-	// 	'0px 0px 60px #d11d4a, inset 0px 0px 60px #d11d4a',
-	// 	180,
-	// 	0,
-	// 	0,
-	// 	1,
-	// 	0.1
-	// );
+	// 		cssTunnelElement.appendChild(newFrame);
+	// 		width = width + 5;
+	// 		// rotateDeg = rotateDeg - 5;
+	// 		// translateY = translateY + 50;
+	// 		translateZ = translateZ - 10;
+	// 		opacity = opacity - 0.05;
+	// 		animationDelay = animationDelay + 0.2;
+	// 	}
+	// }
 });
